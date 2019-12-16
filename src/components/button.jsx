3c98cac511f24f91bf303edee6a7b3aa2e0e51ba
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2019-12-16 13:25:36 -0300

    finalization in 80% from calculator

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import './button.css';
 
-export default Props=>
-    <button className="button">{Props.label}</button>
+export default Props =>
+    <button className={`
+        button
+        ${Props.operation ? 'operation' : ''}
+        ${Props.double ? 'double' : ''}
+        ${Props.triple ? 'triple' : ''}
+    `}
+    onClick={e => Props.click(Props.label)}
+    >
+        {Props.label}
+    </button>
